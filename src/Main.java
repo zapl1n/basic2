a032f8186870c595d7639c258e9c037585bdf52b
Tom <toomas.heljula@khk.ee>
2022-09-07 18:55:47 +0300

    java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[]args){
-        System.out.println("Toomas Heljula");
+        System.out.print("Input the first number:");
+        int a = 30;
+        System.out.print("Input the second number:");
+        int b = 2;
+
+        int d = a/b;
+        System.out.println();
+        System.out.println("The division of a and b is:" +d);
     }
 }
