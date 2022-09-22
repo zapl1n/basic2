094fd55b0a602b8ac00812f670090c5b4990ca46
Tom <toomas.heljula@khk.ee>
2022-09-22 08:59:23 +0300

    java Product

--- src/week4/Product.java
+++ src/week4/Product.java
@@ -1,4 +1,18 @@
 package week4;
 
 public class Product {
+    // price, amount and name
+    private double price;
+    private int amount;
+    private String name;
+
+    public Product(String name, double price, int amount) {
+        this.price = price;
+        this.amount = amount;
+        this.name = name;
+    }
+
+    public void printProduct(){
+        System.out.println(this.name + ", price " + this.price + ", amount " + this.amount);
+    }
 }
