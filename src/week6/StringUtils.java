236df0f1b386e7a71d48aafe3f938775ad427a58
Tom <toomas.heljula@khk.ee>
2022-09-22 09:05:15 +0300

    java StringUtils

--- src/week6/StringUtils.java
+++ src/week6/StringUtils.java
@@ -2,7 +2,6 @@ package week6;
 
 public class StringUtils {
     public static boolean included(String word, String searched){
-        if(word.toUpperCase().contains(searched.trim().toUpperCase());
-        return true;
+        return word.toUpperCase().contains(searched.trim().toUpperCase());
     }
 }
