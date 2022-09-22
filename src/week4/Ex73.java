625aac24515bae45f738867938c782ee2fc505d8
Tom <toomas.heljula@khk.ee>
2022-09-22 08:58:33 +0300

    java ex73

---
+++ src/week4/Ex73.java
@@ -0,0 +1,8 @@
+package week4;
+
+public class Ex73 {
+    public static void main(String[] args) {
+        Product banana = new Product("Banana", 1.1 , 13);
+        banana.printProduct();
+    }
+}
