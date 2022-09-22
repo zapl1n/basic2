14fae7491a9b5e7a44142b07941039dc63d64647
Tom <toomas.heljula@khk.ee>
2022-09-22 09:09:57 +0300

    java Ex4

--- src/week7/Ex4.java
+++ src/week7/Ex4.java
@@ -1,4 +1,20 @@
 package week7;
 
 public class Ex4 {
+    public static void main(String[] Container) {
+        Container container = new Container(1000);
+        addSuitcasesFullOfBricks(container);
+        System.out.println(container);
+    }
+
+    public static void addSuitcasesFullOfBricks(Container container) {
+        // adding 100 suitcases with one brick in each
+        for (int i = 1; i <= 100 ; i++) {
+            Suitcase suitcase = new Suitcase(100);
+            Thing brick = new Thing("Brick", i);
+            suitcase.addThing(brick);
+            container.addSuitcase(suitcase);
+        }
+    }
+
 }
