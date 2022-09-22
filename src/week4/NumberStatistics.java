ff9d885332914f39ae607689d416d5791d1131e9
Tom <toomas.heljula@khk.ee>
2022-09-22 08:59:02 +0300

    java NumberStatistics

---
+++ src/week4/NumberStatistics.java
@@ -0,0 +1,33 @@
+package week4;
+
+public class NumberStatistics {
+    private int amountOfNumbers;
+    private int sum;
+
+    public NumberStatistics() {
+        // initialize here the object variable amountOfNumbers
+        this.amountOfNumbers = 0;
+        this.sum = 0;
+    }
+
+    public void addNumber(int number) {
+        // code here
+        this.sum += number;
+        this.amountOfNumbers++;
+    }
+
+    public int amountOfNumbers() {
+        // code here
+        return this.amountOfNumbers;
+    }
+
+    public int sum() {
+        // code here
+        return this.sum;
+    }
+
+    public double average() {
+        // code here
+        return (double)this.sum / this.amountOfNumbers;
+    }
+}
